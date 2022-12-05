515c1e5b13f70f2a1801e5b53b8d724504de09fd
Denis Stepaniuk <fragett1@Deniss-MacBook-Pro.local>
2022-12-06 02:18:50 +0300

    few changes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import simulation as sm 
 
+#test
+
 sim = sm.Simulation(wind=False, turb_power=0)
 #coef = sim.optimize(opt_type='pd')
 sim.coef = [1.6366748221682974, -4.147750351709235, -2.323712599579363, 2.6427835321523703, 0.0, 0.0]
-sim.run()
+for i in range(10):
+    sim.run()
 sim.graph()
